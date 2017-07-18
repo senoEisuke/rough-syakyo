469ec9f171e339b39542e299e9a055df133801e8
senoEisuke <kazuhiroster@gmail.com>
2017-07-18 10:32:47 +0900

    make hello app

---
+++ electron/electron-readus/main.js
@@ -0,0 +1,21 @@
+'use strinct';
+
+let electron = require('electron');
+let app = electron.app;
+let BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+    if (process.platform != 'darwin')
+        app.quit();
+});
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
